be5ce128418069d69641f8cab8b6669b3e56c418
diegoPaladino <diegopaladinofoto@gmail.com>
2020-09-09 09:23:39 -0300

    202009090923

---
+++ whatsapp_bot.py
@@ -0,0 +1,26 @@
+#whatsapp_bot
+#name_tutorial:Como Criar Um Bot(Robô) No Whatsapp de mensagens P/ Grupos e Contatos
+#link_tutorial:https:\www.youtube.com/watch?v=_ZDBVeqyK6g
+
+#importar as bibliotecas
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+
+#navegar até o whatsapp web
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+operadriver='C://Users//diego//OneDrive//Desktop//DESKTOP//PROGRAMAS//MOZILA//geckodriver.exe'
+# operadriver='C:\Users\diego\OneDrive\Desktop\DESKTOP\PROGRAMAS\MOZILA\geckodriver.exe'
+# chromedriver='C://Users//diego//OneDrive//Desktop//DESKTOP//PROGRAMAS//WEB_DRIVERS//CHROME_DRIVER//chromedriver.exe'
+driver = webdriver.Firefox()
+# driver.get('https:\web.whatsapp.com')
+# time.sleep(30)
+
+#definir contatos, grupos e mensagem a ser enviada
+
+#buscar contatos/grupos
+#enviar mensagens para os contados/grupos
+
+
+
+
